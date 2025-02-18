5d8879abc8e912847a550e20fda39e3e01ab5a64
nabbott <njabbott2005@gmail.com>
2025-02-18 16:02:49 -0600

    need to find something better than this

---
+++ src/element.java
@@ -0,0 +1,15 @@
+/*----------------------------------------------------
+ * Program Title: [InterfaceExample]
+ * Author: Nolan Abbott
+ * Date: [02/18/2025]
+ * Description: [Curious to see what can be done here]
+ * Usage / Issues: not available
+ *-----------------------------------------------------
+ */
+public interface element {
+
+    //Declaring the constants
+
+    //Declaring the abstract methods
+
+}
