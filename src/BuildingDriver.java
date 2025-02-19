9b6dcd096aa30ab58bcba48a5f8efa0128ef9614
ThePresidentofTheUniverse <njabbott2005@gmail.com>
2025-02-19 10:09:06 -0600

    I am done for now, but might add more to it later

--- src/BuildingDriver.java
+++ src/BuildingDriver.java
@@ -1,12 +1,16 @@
 public class BuildingDriver {
     public static void main(String[] args) {
-        Building store1 = new Store("Marfusha's Bakery", "Bakery","Eastern District", 2, "Muffins", 500, 1300, 30,20,30);
+        Building store1 = new Store("Marfusha's Bakery", "Bakery","Eastern District", 2, "Muffins", 500, 1300, 30,20,50);
         Building store2 = new Store("Dochka's Armory", "BlackSmith","Western District", 1,"Swords", 1000, 1700,25,45,30);
         Building amphitheatre1 = new Amphitheatre("Roman Colosseum", "Gladitorial", "Southern District", true, 50000, 1700, 1000,1000,2000);
         Building amphitheatre2 = new Amphitheatre("Hidden Orchard", "theatrical", "Eastern District", false, 2000, 1800, 100, 100, 50);
+        Building house1 = new House("Marfusha's Apartment","Eastern District", "Marfusha",2,30,20,50,true);
+        Building house2 = new House("Dochka's Home", "Western District", "Dochka", 1, 45, 45, 60, false);
 
         store1.whatIs();
+        house1.whatIs();
         store2.whatIs();
+        house2.whatIs();
         amphitheatre1.whatIs();
         amphitheatre2.whatIs();
     }
---
+++ src/House.java
@@ -0,0 +1,54 @@
+public class House implements Building {
+
+    //Fields for a house
+    private String name;
+    private String location;
+    private String owner;
+    private int peopleAMT;
+    private int width;
+    private int length;
+    private int height;
+    private boolean aboveStore;
+
+    //Constructor
+    public House(String name,String location,String owner, int peopleAMT, int width, int length, int height, boolean aboveStore) {
+        this.name = name;
+        this.location = location;
+        this.owner = owner;
+        this.peopleAMT = peopleAMT;
+        this.width = width;
+        this.length = length;
+        this.height = height;
+        this.aboveStore = aboveStore;
+    }
+
+    //Implements methods
+    @Override
+    public String getName() {
+        return name;
+    }
+    @Override
+    public String getLocation() {
+        return location;
+    }
+    @Override
+    public double getWidth() {
+        return width;
+    }
+    @Override
+    public double getLength() {
+        return length;
+    }
+    @Override
+    public double getHeight() {
+        return height;
+    }
+    @Override
+    public void whatIs() {
+        String homeType = (aboveStore ? "an apartment" : "a house");
+        String pluralAMT = (peopleAMT > 1 ? " people.\n" : "; just themselves.\n");
+        String homeLoc = (aboveStore ? "above their store " : "somewhere in the city ");
+        System.out.println(owner + " owns " + homeType + " that houses " + peopleAMT + pluralAMT + "They live " + homeLoc + "in the " + location + ".\n" +
+                "Their home is about " + width + " in width, " + length + " in length and " + height + " in height.\n");
+    }
+}
