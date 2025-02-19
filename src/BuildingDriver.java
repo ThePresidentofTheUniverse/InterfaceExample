8a2faa3109ff28245224e2947844d77ea03dbcbc
ThePresidentofTheUniverse <njabbott2005@gmail.com>
2025-02-19 16:14:15 -0600

    Added random generation to the program

--- src/BuildingDriver.java
+++ src/BuildingDriver.java
@@ -1,7 +1,7 @@
 public class BuildingDriver {
     public static void main(String[] args) {
-        Building store1 = new Store("Marfusha's Bakery", "Bakery","Eastern District", 2, "Muffins", 500, 1300, 30,20,50, new String[]{"test","test2"});
-        Building store2 = new Store("Dochka's Armory", "BlackSmith","Western District", 1,"Swords", 1000, 1700,25,45,30,new String[]{"swords","hammers"});
+        Building store1 = new Store("Marfusha's Bakery", "Bakery","Eastern District", 2, "Muffins", 500, 1300, 30,20,50, "muffin", "loaf of bread", "Doughnut", "cake");
+        Building store2 = new Store("Dochka's Armory", "BlackSmith","Western District", 1,"Swords", 1000, 1700,25,45,30, "sword", "hammer", "axe", "shield");
         Building amphitheatre1 = new Amphitheatre("Roman Colosseum", "Gladitorial", "Southern District", true, 50000, 1700, 1000,1000,2000);
         Building amphitheatre2 = new Amphitheatre("Hidden Orchard", "theatrical", "Eastern District", false, 2000, 1800, 100, 100, 50);
         Building house1 = new House("Marfusha's Apartment","Eastern District", "Marfusha",2,30,20,50,true);
--- src/Store.java
+++ src/Store.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Store implements Building {
 
     //fields for a store
@@ -5,17 +7,20 @@ public class Store implements Building {
     private String storeType;
     private String location;
     private int employees;
-    private String genMerch;
+    private String genMerch = "";
     private int openTime;
     private int closeTime;
     private int width;
     private int length;
     private int height;
-    private String[] merch = {};
+    private String item0;
+    private String item1;
+    private String item2;
+    private String item3;
 
 
     //constructor
-    public Store(String name, String storeType,String location, int employees, String genMerch, int openTime, int closeTime,int width,int length,int height,String[] merch) {
+    public Store(String name, String storeType,String location, int employees, String genMerch, int openTime, int closeTime,int width,int length,int height, String item0, String item1, String item2, String item3) {
         this.name = name;
         this.storeType = storeType;
         this.location = location;
@@ -26,7 +31,10 @@ public class Store implements Building {
         this.width = width;
         this.length = length;
         this.height = height;
-        this.merch = merch;
+        this.item0 = item0;
+        this.item1 = item1;
+        this.item2 = item2;
+        this.item3 = item3;
     }
     //Implement methods
     @Override
@@ -49,15 +57,39 @@ public class Store implements Building {
     public double getHeight() {
         return height;
     }
+
+    //Random needed for item generation.
+    Random randMerch = new Random();
+
     @Override
     public void whatIs() {
         //The boolean that assists with phrasing.
         String empAMT = employees > 1 ? " are " + employees + " employees" : " is " + employees + " employee";
+
+        int merchValue = randMerch.nextInt(0,4);
+
+        switch (merchValue){
+
+            case 0:
+                genMerch = item0;
+                break;
+            case 1:
+                genMerch = item1;
+                break;
+            case 2:
+                genMerch = item2;
+                break;
+            case 3:
+                genMerch = item3;
+                break;
+            default:
+                genMerch = "interesting item";
+                break;
+        };
+
+
         //The full sentences.
         System.out.println(name + " is a " + storeType + " located in the " + location + ".\nThere"+ empAMT + " that might sell you a " + genMerch + " and are open from " + openTime + " to " + closeTime + ".");
         System.out.println("The store's dimensions are " +width+ " in width, " + length + " in length and " + height + " in height.\n");
-        System.out.println("There merchandise includes: " + merch);
     }
-
-
 }
